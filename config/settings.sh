a087bd06f28be67dacce9486b8d0e852d98b6740
Cezar Cruz <cezarcruz22@gmail.com>
2025-06-06 16:44:20 -0300

    Add .env.example file and update .gitconfig to use EMAIL variable; refactor arch.sh and settings.sh for better structure; clean up kitty.conf formatting; remove obsolete locale.conf

--- config/settings.sh
+++ config/settings.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+# This script sets up environment variables for the application
+
+load_settings() {
+  show_info "Setting up environment variables..."
+  # shellcheck disable=SC1091
+  source "$SCRIPT_DIR/.env"
+  show_info "EMAIL=$EMAIL"
+}
+
+
+
