449eef65de5eb1e32e11645948a5758843241b2d
Cezar Cruz <cezarcruz22@gmail.com>
2025-06-06 16:48:18 -0300

    removing

--- config/settings.sh
+++ config/settings.sh
@@ -3,10 +3,15 @@
 
 load_settings() {
   show_info "Setting up environment variables..."
+
+  if file_exists "$SCRIPT_DIR/.env"; then
+    show_info "Loading environment variables from .env file..."
+  else
+    show_error ".env file not found in $SCRIPT_DIR"
+    exit 1
+  fi
+
   # shellcheck disable=SC1091
   source "$SCRIPT_DIR/.env"
   show_info "EMAIL=$EMAIL"
 }
-
-
-
