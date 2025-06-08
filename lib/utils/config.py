62d5b17d1e3581f8965717f4f6194098c87bf60c
Cezar Cruz <cezarcruz22@gmail.com>
2025-06-08 09:26:55 -0300

    Implement configuration management with dry run support; refactor main script to utilize new config class

---
+++ lib/utils/config.py
@@ -0,0 +1,39 @@
+import argparse
+
+from .logger import setup_logger
+
+class Config:
+    def __init__(self):
+        self.dry_run = True
+        self.logger = setup_logger(__name__)
+    
+    def parse_args(self):
+        parser = argparse.ArgumentParser(
+            description='A script to configure Arch Linux post-installation setup.'
+        )
+
+        parser.add_argument(
+            '-dry',
+            '--dry-run', 
+            action='store_true', 
+            default=True,
+            help='Run the script in dry run mode (no changes will be made) (default: True)'
+        )
+
+        parser.add_argument(
+            '-no-dry',
+            '--no-dry-run', 
+            dest='dry_run',
+            action='store_false', 
+            help='Run the script in normal mode (changes will be made)'
+        )
+
+        args = parser.parse_args()
+        self.dry_run = args.dry_run
+
+        return args
+    
+    def is_dry_run(self):
+        return self.dry_run    
+
+config = Config()
--- main.py
+++ main.py
@@ -3,7 +3,12 @@
 import os
 import sys
 from lib.utils.logger import setup_logger
+from lib.utils.config import config
 
+def show_dry_run_warning():
+    if config.is_dry_run():
+        logger.warning("This is a dry run. No changes will be made to the system.")
+        logger.warning("To perform actual changes, set DRY_RUN to False in the script.")
 
 def cleaning_screen():
     os.system('clear' if os.name == 'posix' else 'cls')
@@ -14,14 +19,18 @@ def print_welcome():
     logger.info("Please follow the instructions carefully.")
 
 def main():
+    cleaning_screen()
+
     # check if the script is run with root privileges
     if os.getuid() == 0:
         logger.error("This script should not be run as root. Please run it as a regular user.")
         sys.exit(1)
+    
+    config.parse_args()
 
-    cleaning_screen()
     print_welcome()
+    show_dry_run_warning()
 
 if __name__ == "__main__":
-    logger = setup_logger(__name__)
+    logger = setup_logger(__name__)    
     main()
