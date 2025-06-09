742888fd2b4a053761ef7cdbfad723a6df996d12
Cezar Cruz <cezarcruz22@gmail.com>
2025-06-09 14:40:24 -0300

    formating

--- archmage/lib/utils/base_packages.py
+++ archmage/lib/utils/base_packages.py
@@ -1,31 +1,36 @@
 from archmage.lib.utils.logger import setup_logger
 from archmage.lib.utils.system import default_system
 
-BASE_PACKAGES: list[str] = ["firefox",
-  "docker",
-  "docker-compose",
-  "git",
-  "go",
-  "btop",
-  "neovim",
-  "reflector",
-  "ttf-jetbrains-mono",
-  "pacman-contrib",
-  "bat",
-  "pkgstats",
-  "fish",
-  "ttf-roboto",
-  "fuse",
-  "less",
-  "mise",
-  "rsync",
-  "kitty",
-  "eza"]
+BASE_PACKAGES: list[str] = [
+    "firefox",
+    "docker",
+    "docker-compose",
+    "git",
+    "go",
+    "btop",
+    "neovim",
+    "reflector",
+    "ttf-jetbrains-mono",
+    "pacman-contrib",
+    "bat",
+    "pkgstats",
+    "fish",
+    "ttf-roboto",
+    "fuse",
+    "less",
+    "mise",
+    "rsync",
+    "kitty",
+    "eza",
+]
 
-BASE_FONTS_PACKAGES: list[str] = ["ttf-jetbrains-mono",
-  #"ttf-roboto",
-  "noto-fonts",
-  "ttf-liberation",]
+BASE_FONTS_PACKAGES: list[str] = [
+    "ttf-jetbrains-mono",
+    # "ttf-roboto",
+    "noto-fonts",
+    "ttf-liberation",
+]
+
 
 class BasePackages:
     def __init__(self, logger=None, system=None):
@@ -37,7 +42,6 @@ class BasePackages:
         self.system.install_package(BASE_PACKAGES)
         self.logger.info("Installing fonts...")
         self.system.install_package(BASE_FONTS_PACKAGES)
-
 
 
 default_base_packages: BasePackages = BasePackages()
