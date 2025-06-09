74e00cf54a2737a238657012ad9d757abac91608
Cezar Cruz <cezarcruz22@gmail.com>
2025-06-09 17:29:35 -0300

    adding fontconfig to script

--- archmage/lib/utils/files.py
+++ archmage/lib/utils/files.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 from enum import Enum
 from pathlib import Path
@@ -8,10 +9,12 @@ from archmage.lib.utils.logger import setup_logger
 
 class FilesType(Enum):
     GIT = "git"
+    FONTCONFIG = ".config/fontconfig"
 
 
 PROJECT_ROOT_DIR = str(Path(__file__).parent.parent.parent.parent)
-PROJECT_RESOURCE_DIR = PROJECT_ROOT_DIR + "/resources"
+PROJECT_RESOURCE_DIR = PROJECT_ROOT_DIR + "/resources/"
+HOME_DIR = str(Path.home()) + "/"
 
 
 class Files:
@@ -19,19 +22,27 @@ class Files:
         self.config = config or default_config
         self.logger = logger or setup_logger(__name__)
 
-    def get_asset_path(self, file_name, file_type: FilesType) -> str:
-        match file_type:
-            case FilesType.GIT:
-                return PROJECT_RESOURCE_DIR + "/" + FilesType.GIT.value + "/" + file_name
-
-    def install_asset_in_home(self, path_origin, path_destination) -> None:
-        destination = str(Path.home()) + "/" + path_destination
+    def get_asset_path(self, file_name: str, file_type: FilesType) -> str:
+        return PROJECT_RESOURCE_DIR + file_type.value + "/" + file_name
+    
+    def install_asset_in_home(self, path_origin: str, path_destination: str) -> None:
+        destination = HOME_DIR + path_destination
         
         if self.config.is_dry_run():
             self.logger.info(f"copying from {path_origin} to {destination}")
             return
 
         shutil.copy2(path_origin, destination)
+    
+    def create_dir(self, path_destination: str):
+
+        path = HOME_DIR + path_destination
+
+        if self.config.is_dry_run():
+            self.logger.info(f"creating dir: {path}")
+            return
+        
+        os.makedirs(path, exist_ok=True)
 
 
 __all__ = ["default_files"]
--- archmage/lib/utils/home.py
+++ archmage/lib/utils/home.py
@@ -12,11 +12,16 @@ class Home:
     def configure_user_home(self) -> None:
         self.logger.info("Configuring home directory...")
         self._configure_git_config()
+        self._configure_fontconfig()
 
     def _configure_git_config(self) -> None:
         git_config_file = self.files.get_asset_path(".gitconfig", FilesType.GIT)
         self.files.install_asset_in_home(git_config_file, ".gitconfig")
-
+    
+    def _configure_fontconfig(self) -> None:
+        self.files.create_dir(FilesType.FONTCONFIG.value)
+        fonts_file = self.files.get_asset_path("fonts.conf", FilesType.FONTCONFIG)
+        self.files.install_asset_in_home(fonts_file, FilesType.FONTCONFIG.value + "/fonts.conf")
 
 __all__ = ["default_home"]
 default_home: Home = Home()
