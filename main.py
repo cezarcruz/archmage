61955c56ae8c32478dff6c57e55cf2bd4634eb55
Cezar Cruz <cezarcruz22@gmail.com>
2025-06-09 14:34:36 -0300

    including ruff lint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
 if __name__ == "__main__":
-	from archmage.console.application import Application
-	Application().run()
+    from archmage.console.application import Application
+
+    Application().run()
