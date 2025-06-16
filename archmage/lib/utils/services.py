4d678e67de52ebed742bde52554fca510538bb25
Cezar Cruz <$EMAIL>
2025-06-16 11:25:15 -0300

    fising some start services

--- archmage/lib/utils/services.py
+++ archmage/lib/utils/services.py
@@ -1,4 +1,5 @@
 from archmage.lib.utils.config import default_config
+from archmage.lib.utils.logger import setup_logger
 from archmage.lib.utils.system import default_system
 
 SERVICE_LIST: list[str] = ["docker", "reflector.timer", "fstrim.timer", "paccache.timer"]
@@ -8,8 +9,14 @@ class Services:
     def __init__(self, system=None, config=None, logger=None) -> None:
         self.system = system or default_system
         self.config = config or default_config
+        self.logger = logger or setup_logger(__name__)
 
     def start_common_services(self):
+
+        if self.config.is_dry_run():
+            self.logger.info(f"enabling services {SERVICE_LIST}")
+            return
+
         for service in SERVICE_LIST:
             self.system.enable_service(service)
 
